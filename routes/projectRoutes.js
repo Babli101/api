79938911a3f42e8ce73f71cf4f7114772231c0b0
Babli Sharma <bablithv101@gmail.com.com>
2025-10-29 15:00:31 +0530

    change in projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const Project = require('../models/Project');
+const Project = require('../models/project');
 const path = require('path');
 const fs = require('fs');
 
