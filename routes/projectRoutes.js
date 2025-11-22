3cbc23b5cad6b3c3d324432377bfc1b567a806ec
Babli Sharma <bablithv101@gmail.com.com>
2025-11-22 15:41:59 +0530

    test 3

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -40,11 +40,15 @@ router.post(
       const data = req.body;
 
       const gallery = req.files['gallery']
-        ? req.files['gallery'].map(f => ({
-          url: `/uploads/${f.filename}`,
-          filename: f.originalname
-        }))
+        ? req.files['gallery'].map(f => {
+          const relativePath = f.path.split('uploads')[1];
+          return {
+            url: `/uploads${relativePath.replace(/\\/g, '/')}`,
+            filename: f.originalname
+          };
+        })
         : [];
+
 
       const brochure = req.files['brochure']
         ? {
